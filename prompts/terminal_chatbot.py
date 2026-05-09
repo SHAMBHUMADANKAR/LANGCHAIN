846e33754292c6c5013b54eca7c36f9ceeffd16c
Shambhuling Madankar <shambhulingmadankar@Shambhulings-MacBook-Air.local>
2026-05-09 23:27:13 +0530

    Langchain basics - Message types: Giving LLMs the chat history

--- prompts/terminal_chatbot.py
+++ prompts/terminal_chatbot.py
@@ -1,18 +1,24 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
 chatLLM= ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature= 1.6)
-chat_history = []
+chat_history = [
+    SystemMessage(content='You are a helpful AI assistant')
+]
  
 while True:
     userInput = input('You------: ')
-    chat_history.append(userInput)
+    # chat_history.append(userInput)
+    chat_history.append(HumanMessage(userInput))
     if userInput == 'Exit' :
         break
     result = chatLLM.invoke(chat_history)
-    chat_history.append(result.content)
+    # chat_history.append(result.content)
+    chat_history.append(AIMessage(result.content))
     print('bot---------:', result.content)
 
 print(chat_history)
